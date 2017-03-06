a2ad6547e637d1ccde44fc164617a90e416a32b5
Becca Endres <becca@Beccas-MacBook-Pro-2.local>
2017-03-06 17:41:56 -0600

    url in one line. Source code now correct

--- question1.py
+++ question1.py
@@ -8,9 +8,7 @@ import urllib2
 def main():
 
 	# scrapes given webpage and sets weather_data equal to the source code
-        weather_data = urllib2.urlopen("https://www.wunderground.com/history \
-		/airport/KNUQ/2007/1/1/CustomHistory.html?dayend=31&monthend \
-		=12&year\end=2007&req_city=NA&req_state=NA&req_statename=NA&format=1")
+        weather_data = urllib2.urlopen("https://www.wunderground.com/history/airport/KNUQ/2007/1/1/CustomHistory.html?dayend=31&monthend=12&year\end=2007&req_city=NA&req_state=NA&req_statename=NA&format=1")
 
 	for line in weather_data:
 		print line
