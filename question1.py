18e40c47dea1b119f7bb6179ead76983f27da757
Becca Endres <becca@Beccas-MacBook-Pro-2.local>
2017-03-07 15:16:12 -0600

    Q1: Correct URL. Error fixed

--- question1.py
+++ question1.py
@@ -8,14 +8,13 @@ import urllib2
 def main():
 
 	# scrapes given webpage and sets weather_data equal to the source code
-        weather_data = urllib2.urlopen("https://www.wunderground.com/history/airport/KNUQ/2007/1/1/CustomHistory.html?dayend=31&monthend=12&year\end=2007&req_city=NA&req_state=NA&req_statename=NA&format=1")
+        weather_data = urllib2.urlopen("https://www.wunderground.com/history/airport/KNUQ/2007/1/1/CustomHistory.html?dayend=31&monthend=12&year\end=2007&req_city=NA&req_state=NA&req_statename=NA&format=://www.wunderground.com/history/airport/KNUQ/2007/1/1/CustomHistory.html?dayend=31&monthend=12&yearend=2007&req_city=NA&req_state=NA&req_statename=NA&format=1")
 
 	# In each line of source code looks for February, 2007 and then find
 	# the precipitation by day then adds each day together
-	date = "2007-1"
+	date = "2007-2"
 	total_precip = 0
-	html = weather_data.readlines()
-	for line in html:
+	for line in weather_data:
 		curr_precip = []
 		if date in line:
 			curr_precip = line.split(",")	
