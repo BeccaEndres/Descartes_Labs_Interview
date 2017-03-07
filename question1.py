54bcbcf559570fcbea0b1f27142abecaffd621bd
Becca Endres <becca@Beccas-MacBook-Pro-2.local>
2017-03-06 18:25:37 -0600

    Q1: Adds total precip. Error: only goes through two lines of source code

--- question1.py
+++ question1.py
@@ -10,14 +10,18 @@ def main():
 	# scrapes given webpage and sets weather_data equal to the source code
         weather_data = urllib2.urlopen("https://www.wunderground.com/history/airport/KNUQ/2007/1/1/CustomHistory.html?dayend=31&monthend=12&year\end=2007&req_city=NA&req_state=NA&req_statename=NA&format=1")
 
-	# In each line of source code looks for February, 2007 and then find the precipitation by day and adds them together
-	dates = "2007-2"
-	curr_precip = []
-	for line in weather_data:
-		if dates in line:
-			curr_precip = line.split(",")	
-			print curr_precip[20]
+	
 
+	# In each line of source code looks for February, 2007 and then find the precipitation by day then adds each day together
+	date = "2007-2"
+	total_precip = 0
+	for line in weather_data:
+		curr_precip = []
+		if date in line:
+			curr_precip = line.split(",")	
+			total_pricip += curr_precip[20]
+
+	print "The total precipitation in February 2007 was: %d inches" % total_precip
 
 if __name__ == "__main__":
 	main()
