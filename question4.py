e193b1c3b5562b6e14570d3a0a2c08f97d7d4dbd
Becca Endres <becca@Beccas-MacBook-Pro-2.local>
2017-03-07 01:53:01 -0600

    Q4: Only searches directories that start with the letter l

--- question4.py
+++ question4.py
@@ -5,10 +5,13 @@ import os
 start_here = '/usr'
 loop = 0
 for root, dirs, files in os.walk(start_here):
-    loop += 1
-    print(loop)
-    print('Root is:  %s' % root)
-    if loop >= 20:
-        break
+   if (len(root) > 5):
+       start_letter = root[5].split()    
+       if start_letter == ["l"]:
+           loop += 1
+           print(loop)
+           print('Root is:  %s' % root)
+#          if loop >= 20:
+#             break
 
 
