15176e60b7a5089188a4d05ea964708ffd05723e
Becca Endres <becca@Beccas-MacBook-Pro-2.local>
2017-03-07 01:58:26 -0600

    Q4: Added comments

--- question4.py
+++ question4.py
@@ -5,13 +5,12 @@ import os
 start_here = '/usr'
 loop = 0
 for root, dirs, files in os.walk(start_here):
-   if (len(root) > 5):
-       start_letter = root[5].split()    
-       if start_letter == ["l"]:
-           loop += 1
-           print(loop)
-           print('Root is:  %s' % root)
-#          if loop >= 20:
-#             break
-
+	# checks for initial root that is less than 5 characters 
+	if (len(root) > 5):
+       		# identifies stating letter of directories
+        	start_letter = root[5].split()    
+        	if start_letter == ["l"]:
+                	loop += 1
+                	print(loop)
+                	print('Root is:  %s' % root)
 
