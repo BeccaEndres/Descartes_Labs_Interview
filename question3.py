308b2d8d7107194a562de407083f25a07f2db2ab
Becca Endres <becca@Beccas-MacBook-Pro-2.local>
2017-03-07 01:27:43 -0600

    Q3: Checks for negative numbers and question answered

--- question3.py
+++ question3.py
@@ -9,8 +9,12 @@ def main(t):
     print "seconds since the epoch is %f" % time.time()
 if __name__ == "__main__":
     try:
-        t = int(sys.argv[1])
+       t = int(sys.argv[1])
+       if t < 0:
+	   t = 1
     except:
         t = 1
     main(t)
 
+
+# This script might also fail if the user inputs a large number 
