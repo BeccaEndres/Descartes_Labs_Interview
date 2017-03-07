0b6454d4bcbd67f7d2c0d936c22e948d43440aad
Becca Endres <becca@Beccas-MacBook-Pro-2.local>
2017-03-06 18:05:02 -0600

    Q1: finds precipitation for 1 day in Feb 2007

--- question1.py
+++ question1.py
@@ -10,8 +10,13 @@ def main():
 	# scrapes given webpage and sets weather_data equal to the source code
         weather_data = urllib2.urlopen("https://www.wunderground.com/history/airport/KNUQ/2007/1/1/CustomHistory.html?dayend=31&monthend=12&year\end=2007&req_city=NA&req_state=NA&req_statename=NA&format=1")
 
+	# In each line of source code looks for February, 2007 and then find the precipitation by day and adds them together
+	dates = "2007-2"
+	curr_precip = []
 	for line in weather_data:
-		print line
+		if dates in line:
+			curr_precip = line.split(",")	
+			print curr_precip[20]
 
 
 if __name__ == "__main__":
