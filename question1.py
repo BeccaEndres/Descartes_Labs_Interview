084daac6fc26984a16e97e334169687e3dfdc6c9
Becca Endres <becca@Beccas-MacBook-Pro-2.local>
2017-03-07 15:09:51 -0600

    Q1: Checks case when precip spot in array is empty

--- question1.py
+++ question1.py
@@ -10,16 +10,17 @@ def main():
 	# scrapes given webpage and sets weather_data equal to the source code
         weather_data = urllib2.urlopen("https://www.wunderground.com/history/airport/KNUQ/2007/1/1/CustomHistory.html?dayend=31&monthend=12&year\end=2007&req_city=NA&req_state=NA&req_statename=NA&format=1")
 
-	
-
-	# In each line of source code looks for February, 2007 and then find the precipitation by day then adds each day together
-	date = "2007-2"
+	# In each line of source code looks for February, 2007 and then find
+	# the precipitation by day then adds each day together
+	date = "2007-1"
 	total_precip = 0
-	for line in weather_data:
+	html = weather_data.readlines()
+	for line in html:
 		curr_precip = []
 		if date in line:
 			curr_precip = line.split(",")	
-			total_pricip += curr_precip[20]
+			if curr_precip[19] != "":
+				total_pricip = total_precip +  float(curr_precip[19])	
 
 	print "The total precipitation in February 2007 was: %d inches" % total_precip
 
