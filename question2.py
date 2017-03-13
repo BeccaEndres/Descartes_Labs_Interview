ff68374668c2bf25bb16792d1ffaf1a2a63397a3
Becca Endres <becca@Beccas-MacBook-Pro-2.local>
2017-03-12 23:25:52 -0500

    Q2: Completes covariance of each band individually

--- question2.py
+++ question2.py
@@ -4,14 +4,6 @@
 # 2. compute the covariance matrix of the image bands
 # 3. plot the covariance matrix and save the figure to disk in
 #    png format
-# You may use the initial few lines of code below if you like
-#import dgsamples
-#from osgeo import gdal
-#import numpy as np
-#import matplotlib.pyplot as plt
-#o = gdal.Open(dgsamples.bayou_chip.extract_test)
-#d = o.ReadAsArray()
-
 
 import dgsamples
 from osgeo import gdal
@@ -21,39 +13,19 @@ import matplotlib.pyplot as plt
 def main():
 	o = gdal.Open(dgsamples.bayou_chip.extract_test)
 	
-	# retrieves all bands from image
+	# retrieves all bands from image into 3D array
 	d = o.ReadAsArray()
 
-	i = 0
+	i = 1
+	Cov = []
+
+	# Gets one band at a time and computes it's covariance
 	while i < 9:	
-		band = o.GetRasterBand(i)
-		print band
-		i = i + 1
+		band = o.GetRasterBand(i)	
+		Cov = np.cov(band.ReadAsArray())
+		i += 1
+		
 	
-	block_sizes = band.GetBlockSize()
-	x_block_size = block_sizes[0]
-	y_block_size = block_sizes[1]
-
-	xsize = band.XSize
-	ysize = band.YSize
-
-	print x_block_size
-	print y_block_size
-	print xsize
-	print ysize
-
-	print d.shape	
-
-	cov_matrix = []
-#	for i in d:
-#		print d[i]
-#		cov_matrix = np.cov(d[i])
-	
-
-#	plt.plot(d)
-#	plt.axis('')
-#	plt.show()
-
 if __name__ == "__main__":
 	main()
 
