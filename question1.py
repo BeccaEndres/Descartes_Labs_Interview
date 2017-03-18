078491a95b11c07e07adfd51d429242c4db79231
Becca Endres <becca@Beccas-MacBook-Pro-2.local>
2017-03-18 01:54:56 -0500

    Q1: final cleanup

--- question1.py
+++ question1.py
@@ -7,8 +7,10 @@ import urllib2
 
 def main():
 
+	url = "https://www.wunderground.com/history/airport/KNUQ/2007/1/1/CustomHistory.html?dayend=31&monthend=12&year\end=2007&req_city=NA&req_state=NA&req_statename=NA&format=://www.wunderground.com/history/airport/KNUQ/2007/1/1/CustomHistory.html?dayend=31&monthend=12&yearend=2007&req_city=NA&req_state=NA&req_statename=NA&format=1"
+
 	# scrapes given webpage and sets weather_data equal to the source code
-        weather_data = urllib2.urlopen("https://www.wunderground.com/history/airport/KNUQ/2007/1/1/CustomHistory.html?dayend=31&monthend=12&year\end=2007&req_city=NA&req_state=NA&req_statename=NA&format=://www.wunderground.com/history/airport/KNUQ/2007/1/1/CustomHistory.html?dayend=31&monthend=12&yearend=2007&req_city=NA&req_state=NA&req_statename=NA&format=1")
+        weather_data = urllib2.urlopen(url)
 
 	# In each line of source code looks for February, 2007 and then find
 	# the precipitation by day then adds each day together
